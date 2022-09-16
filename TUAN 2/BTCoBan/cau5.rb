3bcc1f0129e6357e38f1158677f007af3bf76b66
duongmien <duongthimien7@gmail.com>
2022-09-16 20:13:14 +0700

    Update câu 4 + câu 5

---
+++ TUAN 2/BTCoBan/cau5.rb
@@ -0,0 +1,9 @@
+# 5.Viết chương trình để in một số nguyên, một số thực, một ký tự đã được định nghĩa trước.
+
+n = 17
+f = 10.0
+char = "@@@@"
+
+puts "Số nguyên: #{n}"
+puts "Số thực: #{f}"
+puts "Ký tự: #{char}"
