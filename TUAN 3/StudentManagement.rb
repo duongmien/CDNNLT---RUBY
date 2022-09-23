f5aec1ff02a828ae4c15fb95a853fef1b446ebb8
duongmien <duongthimien7@gmail.com>
2022-09-23 11:41:30 +0700

    update opp student manage

---
+++ TUAN 3/StudentManagement.rb
@@ -0,0 +1,58 @@
+class Human
+  attr_accessor :name, :gender, :hairColor, :height, :weight, :phone, :email
+
+  def initialize(name, gender, height, weight, phone, email, hairColor)
+    @name = name
+    @gender = gender
+    @height = height
+    @weight = weight
+    @phone = phone
+    @email = email
+    @hairColor = hairColor
+  end
+
+  def show
+    puts "Name of student: #{name}"
+    puts "Gender of student: #{gender}"
+    puts "Height of student: #{height}cm - Weight of student: #{weight}kg"
+    puts "Hair color of student: #{hairColor}"
+    puts "Contact of student: #{phone} - #{email}"
+  end
+end
+
+class Student < Human
+
+  attr_accessor :id, :_class, :school, :grade
+
+  def initialize(name, gender, height, weight, phone, email, hairColor, id, _class, school, grade)
+    super(name, gender, height, weight, phone, email, hairColor)
+    @id = id
+    @_class = _class
+    @school = school
+    @grade = grade
+  end
+
+  def show
+    super
+    puts "Identification (ID): #{id}"
+    puts "Studying at school: #{school} - #{_class}"
+    puts "Average grade of student: #{grade.round(2)}"
+  end
+end
+student = Student.new("Dương Thị Miên", "Male", 168, 65, "0946794778","duongthimien@gmail.com", "black", "1911505310234", "19T2", "UTE UDN", 9.7256)
+student2 = Student.new("Dương Thị Ngân", "Male", 168, 65, "0946794778","duongthimien@gmail.com", "black", "1911505310234", "19T2", "UTE UDN", 9)
+student3 = Student.new("Dương Anh", "Male", 168, 65, "0946794778","duongthimien@gmail.com", "black", "1911505310234", "19T2", "UTE UDN", 8.78)
+student4 = Student.new("Nguyễn Thị Hồng", "Male", 168, 65, "0946794778","duongthimien@gmail.com", "black", "1911505310234", "19T2", "UTE UDN", 10.0)
+
+stds = [student, student2, student3, student4]
+
+stds = stds.sort_by!{|a| a.grade}
+
+stds = stds.reverse
+
+i = 1
+stds.each do |s|
+  puts "#{i}: #{s.name}"
+  puts s.grade.round(2)
+  i+=1
+end
