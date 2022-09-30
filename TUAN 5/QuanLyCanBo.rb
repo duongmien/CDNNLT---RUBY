010d2cea269fc47cd54102e8846d5cb20401558a
duongmien <duongthimien7@gmail.com>
2022-09-30 10:51:56 +0700

    update code

--- TUAN 5/QuanLyCanBo.rb
+++ TUAN 5/QuanLyCanBo.rb
@@ -1,4 +1,4 @@
-class CanBo
+class Cadres
   attr_accessor :name, :gender, :age, :address
 
   def initialize(name, gender, age, address)
@@ -16,7 +16,7 @@ class CanBo
   end
 end
 
-class CongNhan < CanBo
+class Worker < Cadres
   attr_accessor :level
   def initialize(name, gender, age, address,level)
     super(name, gender, age, address)
@@ -26,8 +26,8 @@ class CongNhan < CanBo
     super
     puts "Level of employee: #{level}"
   end
-
-class KySu < CanBo
+end
+class Engineer < Cadres
   attr_accessor :faculity
   def initialize(name, gender, age, address,faculity)
     super(name, gender, age, address)
@@ -37,8 +37,8 @@ class KySu < CanBo
     super
     puts "Faculity of employee: #{faculity}"
   end
-
-class NhanVien < CanBo
+end
+class Staff  < Cadres
   attr_accessor :job
   def initialize(name, gender, age, address,job)
     super(name, gender, age, address)
@@ -48,5 +48,37 @@ class NhanVien < CanBo
     super
     puts "Job of employee: #{job}"
   end
+end
+class ManagerCadres
+  @@cadres = Array.new
+    def addCadres(cadres)
+        @@cadres.push(cadres)
+    end
+    
+    def searchCadresByName(key)
+        searchList = @@cadres.select {|o| o.name.include? key}
+        searchList.each{ |sl| sl.show}
+    end
+
+    def showListInforCadres()
+        @@cadres.each{ |cadres| cadres.show}
+    end
+end
+
+c1 = Cadres.new("Duong Thi Mien", 24,"Nu", 'Quang Nam')
+c2 = Cadres.new("Duong Thi Ngan", 24,"Nu", 'Quang Nam')
+c3 = Cadres.new("Duong Anh", 44,"Nam", 'Quang Nam')
+c4 = Cadres.new("Nguyen Thu Hong", 44,"Nam", 'Da Nang')
+c5 = Cadres.new("Duong Hien An", 14,"Nam", 'Da Nang')
+
+list = ManagerCadres.new()
+list.addCadres(c1)
+list.addCadres(c2)
+list.addCadres(c3)
+list.addCadres(c4)
+list.addCadres(c5)
+
+# list.showListInforCadres()
+list.searchCadresByName("Duong Thi Mien")
 
 
